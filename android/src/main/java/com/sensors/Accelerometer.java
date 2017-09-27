7c65e9b1a42d62474a0598f8853b6af32ebebf77
Jiaming Lu <jiaming.lu@ele.me>
2017-09-27 11:52:59 +0800

    throw error by promise, instead of uncatchable crash

--- android/src/main/java/com/sensors/Accelerometer.java
+++ android/src/main/java/com/sensors/Accelerometer.java
@@ -14,7 +14,9 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
+
 
 public class Accelerometer extends ReactContextBaseJavaModule implements SensorEventListener {
 
@@ -39,10 +41,10 @@ public class Accelerometer extends ReactContextBaseJavaModule implements SensorE
   }
 
   @ReactMethod
-  public void startUpdates() {
+  public void startUpdates(Promise promise) {
     if (this.sensor == null) {
       // No sensor found, throw error
-      throw new RuntimeException("No Accelerometer found");
+      promise.reject(new RuntimeException("No Accelerometer found"));
     }
     // Milisecond to Mikrosecond conversion
     sensorManager.registerListener(this, sensor, this.interval * 1000);
